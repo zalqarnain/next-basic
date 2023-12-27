e9143dd696871d787475f3e5a7ad3972cfea7a57
zalqarnain <zalqarnainahsen03@gmail.com>
2023-12-27 21:53:47 +0500

    done

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,14 +1,24 @@
+"use client"
 import Link from 'next/link';
 import React from 'react';
-
+import { usePathname } from 'next/navigation';
 function Navbar() {
+    const pathName=usePathname()
     return (
+       <>
+        {
+            pathName!=="/login" ?
         <>
-        <Link href='/'>Home</Link>
-        <Link href='/tickets'>Tickets</Link>
-        <Link href='/home/about'>About</Link>
+        <Link href='/'><button>Home</button></Link>
+        <Link href='/tickets'><button>Tickets</button></Link>
+        <Link href='/home/about'><button>About</button></Link>
+        <Link href='/login'><button>Login</button></Link>
 
         </>
+        :
+        <></>
+        }
+       </>
     );
 }
 
---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login(props) {
+    return (
+        <div>
+            <p>Login page</p>
+        </div>
+    );
+}
+
+export default Login;
