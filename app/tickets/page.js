3b5ff459866ceb11de659ee04328dc46972c4f8c
zalqarnain <zalqarnainahsen03@gmail.com>
2023-12-27 11:24:06 +0500

    done

--- app/tickets/page.js
+++ app/tickets/page.js
@@ -1,10 +1,19 @@
-import Head from 'next/head';
+"use client"
+import { useRouter } from 'next/navigation';
 import React from 'react';
-
 function Tickets(props) {
+
+    const router = useRouter();
+
+    function handleChange() {
+        router.push('/home/about')
+    }
+
     return (
         <div>
             <p>Tickets page</p>
+            <br />
+            <button onClick={handleChange}>Navigate</button>
         </div>
     );
 }
